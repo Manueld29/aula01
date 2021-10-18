fbe3c737f284c732318ac3b82605bdf31671b606
13124 <13124@enautica.pt>
2021-10-18 17:40:19 +0100

    Aula

--- src/ex1_c.java
+++ src/ex1_c.java
@@ -5,11 +5,23 @@ public class ex1_c {
     public static void main(String[] args){
         int a;
         float b,x,y,z;
-        Scanner ab = new Scanner(System.in);
+
+        Scanner xx = new Scanner(System.in);
         System.out.println("Numero 1");
-        a = ab.nextInt();
+        x = xx.nextInt();
+
+        Scanner yy = new Scanner(System.in);
+        System.out.println("Numero 2");
+        y = yy.nextInt();
+
+        Scanner zz = new Scanner(System.in);
+        System.out.println("Numero 3");
+        z = zz.nextInt();
+
+        Scanner aa = new Scanner(System.in);
+        System.out.println("Numero 4");
+        a = aa.nextInt();
         b = a;
-        System.out.println("%f.2",b);
 
 
     }
